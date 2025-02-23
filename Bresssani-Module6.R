dd7147fe48c7e8a094e150c6bff95128d7da4f70
James Bressani <bressanijames@gmail.com>
2025-02-22 21:24:56 -0500

    Added Bresssani-Module6.R

---
+++ Bresssani-Module6.R
@@ -0,0 +1,17 @@
+A <- matrix(c(2, 0, 1, 3), ncol = 2)
+B <- matrix(c(5, 2, 4, -1), ncol = 2)
+
+A
+B
+(A+B)
+(A-B)
+
+matrix1 <- diag(c(4,1,2,3))
+matrix1
+
+matrix2 <- diag(3,5)
+matrix2
+matrix2 [1, 2:5] <-1
+matrix2[2:5, 1] <- 2
+matrix2
+
